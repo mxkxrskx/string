8538522e02774d4de6cc18e5ce676692a1ecba66
Abel Axell <abelaxel@si-k3.kzn.21-school.ru>
2024-05-16 12:34:08 +0300

    abelaxel fix incorrect pointer

--- src/s21_func/s21_strncat.c
+++ src/s21_func/s21_strncat.c
@@ -2,10 +2,13 @@
 
 char *s21_strncat(char *dest, const char *src, s21_size_t n) {
   char *ptr = dest;
-  while (*ptr++);
-  while (*src++ && n > 0) {
+  while (*ptr) {
+    ptr++;
+  }
+  while (*src && n > 0) {
     *ptr = *src;
     ptr++;
+    src++;
     n--;
   }
   *ptr = '\0';
