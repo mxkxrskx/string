d785772be4251183677eb6fa6f808a01438a34cb
Coriander Miyu <coriandm@si-j2.kzn.21-school.ru>
2024-05-16 23:30:56 +0300

    изменил цикл фор, теперь он работает с любым количеством аргументов

--- src/s21_string_main.c
+++ src/s21_string_main.c
@@ -1,10 +1,9 @@
 #include "testFunc/s21_string_test.h"
-#define COUNT 9
 
 
 int main(){
     Suite *suite_array[] = {suite_memchr(), suite_memcmp(), suite_memcpy(), suite_strerror(), suite_strlen(), suite_strncat(), suite_memset(), suite_strchr(), suite_strpbrk()};
-    for(int i = 0; i < COUNT; i++){
+    for(unsigned long i = 0; i < sizeof(suite_array) / sizeof(suite_array[0]); i++){
         run_test_cases(suite_array[i]);
         printf("\n");
     }
