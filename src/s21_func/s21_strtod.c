25ae8943da4cace3878df6bdcecea5bc44162f54
Abel Axell <abelaxel@si-k3.kzn.21-school.ru>
2024-05-17 14:56:58 +0300

    strtod

---
+++ src/s21_func/s21_strtod.c
@@ -0,0 +1,33 @@
+#include "s21_strtod.h"
+
+float parse_float(char *ptr, char **endptr) {
+  int sign = get_sign(&ptr);
+  float num = s21_strtol(ptr, &ptr, BASE_DECIMAL);
+  if (*ptr == '.') {
+    ptr++;
+    char *ptr_start = ptr;
+    int decimal_digits = s21_strtol(ptr, &ptr, BASE_DECIMAL);
+    int decimal_exp = ptr - ptr_start;
+    num += decimal_digits * powf(10.0f, -decimal_exp);
+  }
+  if (endptr) {
+    *endptr = ptr;
+  }
+  return sign * num;
+}
+
+float s21_strtod(const char *str, char **endptr) {
+  char *ptr = (char *)str;
+  int sign = get_sign(&ptr);
+  float mantissa = parse_float(ptr, &ptr);
+  int exponent = 0;
+  if (*ptr == 'e' || *ptr == 'E') {
+    ptr++;
+    exponent = s21_strtol(ptr, &ptr, BASE_DECIMAL);
+  }
+  float result = mantissa * powf(10.0f, exponent);
+  if (endptr) {
+    *endptr = ptr;
+  }
+  return sign * result;
+}
