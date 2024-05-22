b0a80a1f66b52b4bd7b2eea28b238007cea0ec68
Kandice Action <kandicea@si-k5.kzn.21-school.ru>
2024-05-22 14:19:40 +0300

    nothing

--- src/s21_func/s21_sprintf.c
+++ src/s21_func/s21_sprintf.c
@@ -5,19 +5,18 @@ typedef struct Specifiers{
     double f;
     char *s;
     unsigned int u;
-
-    char *argv[BUFF];
-    int len;
-    int i_len;
 } Specifiers;
 
-//Ширина: (число)
-//Точность: .(xbckj)
-//Длина: h,l
+
+bool s21_isdigit(char c){
+    return (c >= '0' && c <= '9');
+}
+
 
 void getNUMfromString(int num, int base, Specifiers* spec){
     char tmp[BUFF] = "\0";
     int index = BUFF - 2;
+    int temp = num;
     num = num > 0 ? num : -num;
     if(num == 0){
         tmp[index] = '0';
@@ -27,31 +26,51 @@ void getNUMfromString(int num, int base, Specifiers* spec){
         tmp[index] = "0123456789abcdef"[num%base];
         num/= base;
     }
+    if(temp < 0){
+        tmp[--index] = '-';
+    }
+    //поменять на свой
     strcat(spec->argv[spec->i_len++], tmp+index);
+}
+void getFLOATfromString(double f, Specifiers *spec){
+    long double left = 0.0;
+    long double right = modfl(f, &left);
+    getNUMfromString((int)left, 10, spec);
+    spec->i_len--;
+    int count = 0;
+    while(count != 6){
+        right*=10;
+        count++;
+    }
+    strcat(spec->argv[spec->i_len], ".");
+    getNUMfromString((int)right, 10, spec);
 }
 
 void set_spaces(char *format, int *spaces, va_list args){
-    format--;
-    if(*(format) == '*'){
+    char *tmp = format;
+    tmp++;
+    if(*(tmp) == '*'){
         *spaces = va_arg(args, int);
     }
-    else if(isdigit(*format) && *format-- != '.'){
-
-        char* tmp = format;
-        while((isdigit(*tmp))){
+    else if(isdigit(*tmp) && *tmp++ != '.'){
+        while((s21_isdigit(*tmp))){
             tmp--;
         }
         *spaces = s21_atoi(++tmp);
     }
 }
 
-void procces_width_d(int spaces, Specifiers *spec){
-    if((spaces -= (int)log10(spec->d)+1) > 0){
-        s21_memset(spec->argv[spec->i_len], ' ', spaces);
-        spec->argv[spec->i_len][spaces+1] = '\0';
-    }
+
+void input_spaces(Specifiers *spec, int spaces){
+    s21_memset(spec->argv[spec->i_len], ' ', spaces);
+    spec->argv[spec->i_len][spaces+1] = '\0';
 }
 
+void is_negative(int num, int *spaces){
+    if(num < 0.0){
+        *spaces += 1;
+    }
+}
 int getSpecFromLine(char *format, va_list args, Specifiers *spec){
     bool flag = 1;
     char *tmp = format;
@@ -65,16 +84,13 @@ int getSpecFromLine(char *format, va_list args, Specifiers *spec){
         case 'd':
             set_spaces(tmp, &spaces, args);
             spec->d= va_arg(args, int);
-            procces_width_d(spaces, spec);
+            is_negative(spec->d, &spaces);
             getNUMfromString(spec->d, 10, spec);
-            spaces = 0;
             break;
         case 'f':
             set_spaces(tmp, &spaces, args);
-            //не работает 3.14 -> 3.140000
-            //printf("%f", spec->f);
             spec->f = va_arg(args, double);
-            printf("%f", spec->f);
+            getFLOATfromString(spec->f, spec);
             break;
         case 's':
             spec->s = va_arg(args, char*);
@@ -84,6 +100,7 @@ int getSpecFromLine(char *format, va_list args, Specifiers *spec){
             set_spaces(tmp, &spaces, args);
             spec->u = va_arg(args, unsigned int);
             getNUMfromString(spec->u, 10, spec);
+
             break;
         default:
             flag = 0;
@@ -100,18 +117,6 @@ void getSpecFromFormat(const char *format, va_list args, Specifiers *spec){
     }
 }
 
-void fillArr(char *argv[], int count){
-    for(int i = 0; i < count; i++){
-        argv[i] = (char *)calloc(BUFF, sizeof(char));
-    }
-}
-
-void freeArr(char *argv[], int count){
-    for(int i = 0; i < count;i++){
-        free(argv[i]);
-    }
-}
-
 int count_spec(const char *format){
     int count = 0;
     for(s21_size_t i = 1; i < s21_strlen(format); i++){
@@ -122,21 +127,18 @@ int count_spec(const char *format){
     return count;
 }
 
-void praseArgs(const char *format, va_list args){
-    Specifiers spec = {0};
-    spec.len= count_spec(format);
-    fillArr(spec.argv, spec.len);
-    for(;*format++;){
-        if(*format == '%'){
-            getSpecFromFormat(format, args, &spec);
-        }
-    }
-    for(int i = 0; i < spec.len; i++){
-        printf("%s\n", spec.argv[i]);
-    }
+// void praseArgs(const char *format, va_list args){
+//     for(;*format++;){
+//     }
 
-    freeArr(spec.argv, spec.len);
-}
+
+
+//     for(int i = 0; i < spec.len; i++){
+//         printf("%s = %lu\n", spec.argv[i], s21_strlen(spec.argv[i]));
+//     }
+
+//     freeArr(spec.argv, spec.len);
+// }
 
 
 //Подставить вместо спецификатора наш input
@@ -150,35 +152,33 @@ void praseArgs(const char *format, va_list args){
 //Ширина: (число)
 //Точность: .(xbckj)
 //Длина: h,l
+//* 5678d
 
 int s21_sprintf(char *str, const char *format, ...){
     va_list args;
     va_start(args, format);
-    praseArgs(format, args);
 
-
+    char temp[BUFF] = "\0";
+    int spaces = 0;
+    for(;*format;){
+        if(*format == '%'){
+            set_spaces(format, &spaces, args);
+        }
+    }
     va_end(args);
-
 
     return 0;
 }
 
 
 int main() {
-    char name[] = "Makar";
-    int age =  20;
+    int age =  -20;
     char str1[BUFF] = "\0";
-    sprintf(str1, "Name: %s, Age: %5d, float = %10.1f %c", name, age, 3.0, '!');
-    printf("%s", str1);
-    
-    
-    // long double pi = 3.14;
-    // printf("%Lf\n", pi);
+    s21_sprintf(str1, "Age = %d %u %5f", age, 20, 3.14);
+    //sprintf(str1, "%4d", age);
+    //printf("%s", str1);
 
-    // long double left = 0.0;
-    // long double right = modfl(pi, &left);
-    // printf("%d, %Lf\n", (int)left, right);
-    // while
+    
     return 0;
 
 }
