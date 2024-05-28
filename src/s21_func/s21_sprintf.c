155e65c25a1a4a853a179f902d4b7202bea71ab3
Kandice Action <kandicea@si-k5.kzn.21-school.ru>
2024-05-28 20:31:37 +0300

    fix bugs and сделал spec d and c

--- src/s21_func/s21_sprintf.c
+++ src/s21_func/s21_sprintf.c
@@ -1,30 +1,28 @@
-#include "s21_char_utils.h"
-#include "s21_parse_number.h"
-#include "s21_string.h"
-
+#include "s21_sprintf.h"
 
 int main() {
-  char str1[BUFF] = "\0";
-  s21_sprintf(str1, "%- +024d strstrstr%c", -51, '1');
-  printf("%s", str1);          
+  Suite *suite_array[] = {suite_sprintf_flags_d(), suite_sprintf_flags_c()};
+  for (unsigned long i = 0; i < sizeof(suite_array) / sizeof(suite_array[0]);
+       i++) {
+    run_testcase(suite_array[i]);
+    printf("\n");
+  }
+  // char str[BUFF] = "\0";
+  // s21_sprintf(str, "%-10c", 'B');
+  // printf("%s", str);
+
+
 
   return 0;
 }
 
+void run_testcase(Suite *testcase){
+  SRunner *sr = srunner_create(testcase);
+  srunner_set_fork_status(sr, CK_NOFORK);
+  srunner_run_all(sr, CK_NORMAL);
 
-// Спецификаторы PART 2: c d f s u
-typedef struct Specifiers {
-  bool space;
-  bool zero;
-  bool minus;
-  bool plus;
-  
-  char length;
-  int width;
-  int precision;
-
-
-} Specifiers;
+  srunner_free(sr);  
+}
 
 // void getFLOATfromString(double f, Specifiers *spec){
 //     long double left = 0.0;
@@ -40,23 +38,14 @@ typedef struct Specifiers {
 //     getNUMfromString((int)right, 10, spec);
 // }
 
-// void input_spaces(Specifiers *spec, int spaces){
-//     s21_memset(spec->argv[spec->i_len], ' ', spaces);
-//     spec->argv[spec->i_len][spaces+1] = '\0';
-// }
-
-void is_negative(int num, int *spaces) {
-  if (num < 0.0) {
-    *spaces += 1;
-  }
-}
 
 void set_flags(const char *format, Specifiers *spec) {
     char *tmp = (char *)format;
     tmp++;
     while(1){
+      char *tmp2 = tmp;
       if (*tmp == ' ') spec->space = true;
-      else if(*tmp == '0') spec->zero = true;
+      else if(*tmp == '0' && !is_digit(*(--tmp2))) spec->zero = true;
       else if(*tmp == '-') spec->minus = true;
       else if(*tmp == '+') spec->plus = true;
       else{
@@ -65,8 +54,11 @@ void set_flags(const char *format, Specifiers *spec) {
       tmp++;
       if(s21_strchr("cdieEfgGosuxXpn", *tmp) || (is_digit(*tmp) && *tmp != '0') || *tmp == '.') break;
     }
-    if(spec->minus && spec->zero){
+    if((spec->minus == 1 && spec->zero == 1)){
       spec->zero = 0;
+    }
+    if(spec->plus == 1 && spec->space == 1){
+      spec->space = 0;
     }
 }
 
@@ -88,7 +80,7 @@ void set_width(const char *format, va_list args, Specifiers *spec) {
 
 void set_precision(const char *format, va_list args, Specifiers *spec) {
   char *tmp = (char *)format + 1;
-  while (!s21_strchr("cdfsu", *tmp)) {
+  while (!s21_strchr("cdieEfgGosuxXpn", *tmp)) {
     if (*tmp == '.') {
       tmp++;
       if (*tmp == '*') {
@@ -113,14 +105,39 @@ void set_length(const char *format, Specifiers *spec) {
   }
 }
 
-void process_c(va_list args, Specifiers *spec, char temp[BUFF]){
+void process_line_with_condition(char str1[BUFF], char str2[BUFF], Specifiers *spec){
+  s21_size_t width = spec->width - s21_strlen(str2);
+  if(spec->zero && width > 0 && spec->precision == 0){
+    getSTRING(str1, str2, '0', width);
+  }
+  else if(spec->minus && width > 0){
+    minus_process(str1, str2, spec, width);
+  }
+  else if(width > 0 && spec->width != 0){
+    getSTRING(str1, str2, ' ', width);
+  }
+  else{
+    s21_strncat(str1, str2, s21_strlen(str2));
+  }
+}
+
+void get_line_c(wchar_t c, Specifiers *spec, char str[BUFF]){
+  char c_line[BUFF] = "\0";
+  c_line[0] = c;
+  if(spec->space){
+    spec->space = 0;
+  }
+  process_line_with_condition(str, c_line, spec);
+}
+
+void process_c(va_list args, Specifiers *spec, char str[BUFF]){
     if(spec->length == 'l'){
         wchar_t c = va_arg(args, wchar_t);
-        //пробелы минусы нули
+        get_line_c(c, spec, str);
     }
     else{
         char c = va_arg(args, int);
-        //пробелы минусы нули
+        get_line_c(c, spec, str);
     }
 }
 
@@ -137,31 +154,35 @@ void getSTRINGfromNUM(int64_t num, int base, char tmp1[], Specifiers *spec){
         tmp[index] = "0123456789abcdef"[num%base];
         num/= base;
     }
+    s21_size_t precision = spec->precision - s21_strlen(tmp+index);
+    if(precision > 0 && spec->precision != 0){
+      while(precision > 0){
+        tmp[--index] = '0';
+        precision--;
+      }
+    }
     if(temp < 0){
         tmp[--index] = '-';
     }
-    if(spec->plus && spec->zero && temp > 0){
+    if(spec->plus && spec->zero == 0 && temp > 0){
       tmp[--index] = '+';
     }
     s21_strncat(tmp1, tmp+index, s21_strlen(tmp+index));
 }
 
-void getSTRING(char str1[BUFF], char str2[BUFF], Specifiers *spec, char c) {
-    int index = 0;
-    if (str2[0] == '-' && c == '0') {
-        str1[index++] = '-';
-    }
-    s21_size_t width = spec->width - s21_strlen(str2);
-    s21_strncpy(str1+index+width,str2+index, s21_strlen(str2+index));
-    if(spec->zero && spec->space && str2[0] != '-'){
-      str1[0] = ' ';
-    }
-    if(spec->zero && spec->plus && str2[0] != '-'){
-      str1[0] = '+';
-    }
+void getSTRING(char str1[BUFF], char str2[BUFF], char c, s21_size_t width) {
+    s21_memset(str1, c, width);
+    s21_strncpy(str1+width,str2, s21_strlen(str2));
 }
 
-void process_d(va_list args, Specifiers *spec, char str1[BUFF]){
+void minus_process(char str1[BUFF], char str2[BUFF], Specifiers *spec, s21_size_t width){
+  s21_memset(str2+s21_strlen(str2), ' ', width);
+  s21_strncpy(str1, str2, s21_strlen(str2)-spec->space);
+}
+
+
+
+void process_d(va_list args, Specifiers *spec, char str[BUFF]){
     int64_t d = va_arg(args, int64_t);
     switch(spec->length){
         case 0:
@@ -173,20 +194,19 @@ void process_d(va_list args, Specifiers *spec, char str1[BUFF]){
     }
     char number[BUFF] = "\0";
     getSTRINGfromNUM(d, 10, number, spec);
-    bool condition = s21_strlen(number) < (s21_size_t)spec->width;
-    printf("zero = %d, space = %d, plus = %d width = %d\n", spec->zero, spec->space, spec->plus, spec->width);
-    if(spec->zero && condition){
-      getSTRING(str1, number, spec, '0');
+    if(number[0] != '-' && spec->space == 1 && spec->minus == 1){
+      str[0] = ' ';
+      str++;
     }
-    else if(spec->minus && condition){
-      s21_memset(number+s21_strlen(number), ' ', spec->width-s21_strlen(number));
-      s21_strncpy(str1, number, s21_strlen(number));
+    if(spec->space == 1 && number[0] == '-'){
+      spec->space = 0;
     }
-    else if(condition && spec->width != 0){
-      getSTRING(str1, number, spec, ' ');
+    process_line_with_condition(str, number, spec);
+    if(spec->zero && spec->space && number[0] != '-'){
+      str[0] = ' ';
     }
-    else{
-      s21_strncat(str1, number, s21_strlen(number));
+    if(spec->zero && spec->plus && number[0] != '-' && spec->precision == 0){
+      str[0] = '+';
     }
 }
 
@@ -198,16 +218,16 @@ void set_spec(const char *format, Specifiers *spec, va_list args,  char str[BUFF
   switch (*tmp) {
     case 'c':
         process_c(args, spec, str);
-        break;
+        break; 
     case 'd':
         process_d(args, spec, str);
-        printf("%s", str);
         break;
     case 'f':
         break;
     case 's':
         break;
     case 'u':
+        process_d(args, spec, str);
         break;
     }
 }
@@ -236,6 +256,7 @@ int s21_sprintf(char *str, const char *format, ...) {
       set_precision(format, args, &spec);
       set_length(format, &spec);
       set_spec(format, &spec, args, temp);
+      s21_strncat(str, temp, s21_strlen(temp));
       skip_spec_line(&format);
     }
     if(*format == '\0'){
