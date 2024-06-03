847b6f3aee514215b9653383a53a8378ae3689af
Foetid Pond <foetidpo@un-j5.kzn.21-school.ru>
2024-06-03 19:53:44 +0300

    add strcspn func and strcspn tests

--- src/s21_func/s21_string.h
+++ src/s21_func/s21_string.h
@@ -36,6 +36,7 @@ char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 int s21_sprintf(char *str, const char *format, ...);
 int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
 char *s21_strtok(char *str, const char *delim);
+s21_size_t s21_strcspn(const char *str1, const char *str2);
 void *s21_to_lower(const char *str);
 void *s21_to_upper(const char *str);
 void *s21_trim(const char *src, const char *trim_chars);
